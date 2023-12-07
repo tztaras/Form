4153397419c272d48c0ce8360fe758bbc472a28a
tarasend <tz.taras@gmail.com>
2023-12-07 12:22:36 +0200

    alpha form css

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 
 console.log('DOGE');
 
-let sw = document.querySelector('.switch').onclick = f1;
-function f1(action) {
-    console.log(action)
+let sw = document.querySelector('.switch').onclick = alphaform;
+function alphaform(action) {
+    console.log(action);
+    let b = document.querySelector('body');
+    b.classList.toggle('alphaform');
+
+    let f = document.querySelector('form');
+    f.classList.toggle('form-alphaform');
+
+    let i = document.querySelectorAll('input');
+    i.forEach((ii) => {
+        ii.classList.toggle('input-alpha');
+    });
+    
+    let rb = document.querySelectorAll('.rad');
+    rb.forEach((rborange) => {
+        rborange.classList.toggle('rad-alpha')
+    });
+
 }
