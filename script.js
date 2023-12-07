2d3d43118297551bfa83ad100a09d6aac0483c69
tarasend <tz.taras@gmail.com>
2023-12-07 20:55:05 +0200

    toggle between dogos

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
 
 console.log('DOGE');
-
+let doge = 'weakdoge.png';
+let dogeImage = document.getElementById('dogeImage');
 let sw = document.querySelector('.switch').onclick = alphaform;
 function alphaform(action) {
+
+
     console.log(action);
+
+    let a = 'ALPHA FORM';
+    let s = 'Simplɘ form';
+    
+    let h1 = document.querySelector('h1');
+        if (h1.textContent === a) {
+            h1.textContent = s;
+
+    } else {
+            h1.textContent = a;
+    }
+  
+    
+    
+    doge = (doge === 'weakdoge.png') ? 'strongdoge.jpg' : 'weakdoge.png';
+    dogeImage.src = './img/' + doge;
+    
+    // changeDoge('./img/strongdoge.jpg');
+
+    
+    
+ 
+
+   
     let b = document.querySelector('body');
     b.classList.toggle('alphaform');
 
@@ -11,13 +38,36 @@ function alphaform(action) {
     f.classList.toggle('form-alphaform');
 
     let i = document.querySelectorAll('input');
-    i.forEach((ii) => {
+    i.forEach( (ii) => {
         ii.classList.toggle('input-alpha');
     });
     
     let rb = document.querySelectorAll('.rad');
-    rb.forEach((rborange) => {
+    rb.forEach( (rborange) => {
         rborange.classList.toggle('rad-alpha')
     });
 
 }
+
+
+// function changeDoge(img) {
+//         document.querySelector('#dogeImage').setAttribute('src', img);
+        
+//     }
+    
+
+
+// document.querySelector('#switchform').addEventListener('click', function () {
+//     dogeToggle();
+// });
+
+// function dogeToggle() {
+//     let dogetype = document.querySelector('#dogeImage');
+//      console.log('Current Image Source:', dogetype.src);
+//     if (dogetype.src.endsWith('./img/strongdoge.jpg')) {
+//         changeDoge('./img/weakdoge.png');
+//     } else {
+//         changeDoge('./img/strongdoge.jpg');
+//     }
+//     console.log('New Image Source:', dogetype.src);
+// }
